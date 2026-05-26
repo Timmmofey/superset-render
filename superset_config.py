1a4d0f8721fcc7dc89b73e8a0b81a413f704b5fc
Timmmofey <132023373+Timmmofey@users.noreply.github.com>
2026-05-26 17:19:50 +0300

    Update superset_config.py

--- superset_config.py
+++ superset_config.py
@@ -1,15 +1,5 @@
 import os
 
-def get_env_var(name, default=None):
-    value = os.environ.get(name, default)
-    if value is None:
-        raise Exception(f"Переменная {name} не установлена!")
-    return value
-
-SECRET_KEY = get_env_var('SECRET_KEY')
-# Явно указываем драйвер psycopg2
-raw_url = get_env_var('DATABASE_URL')
-if raw_url.startswith('postgresql://'):
-    raw_url = raw_url.replace('postgresql://', 'postgresql+psycopg2://', 1)
-SQLALCHEMY_DATABASE_URI = raw_url
+SECRET_KEY = os.environ.get('SECRET_KEY', 'temp-secret-key-change-me')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
