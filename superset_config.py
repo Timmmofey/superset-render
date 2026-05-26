bf43bc6c6221db802ada8fb8a496226817c6fdec
Timmmofey <132023373+Timmmofey@users.noreply.github.com>
2026-05-26 17:31:33 +0300

    Update superset_config.py

--- superset_config.py
+++ superset_config.py
@@ -1,9 +1,8 @@
 import os
 
-SECRET_KEY = os.environ.get('SECRET_KEY', 'temp-secret-key')
-# Явно указываем драйвер psycopg2
-DATABASE_URL = os.environ.get('DATABASE_URL')
-if DATABASE_URL and DATABASE_URL.startswith('postgresql://'):
-    DATABASE_URL = DATABASE_URL.replace('postgresql://', 'postgresql+psycopg2://', 1)
-SQLALCHEMY_DATABASE_URI = DATABASE_URL
+SECRET_KEY = os.environ.get('SECRET_KEY', 'temporary-secret-key')
+raw_url = os.environ.get('DATABASE_URL')
+if raw_url and raw_url.startswith('postgresql://'):
+    raw_url = raw_url.replace('postgresql://', 'postgresql+psycopg2://', 1)
+SQLALCHEMY_DATABASE_URI = raw_url
 SQLALCHEMY_TRACK_MODIFICATIONS = False
