877906f821a05de63690e4a8a9ae2c9e3eda7b9b
Timmmofey <132023373+Timmmofey@users.noreply.github.com>
2026-06-27 18:38:33 +0300

    Update superset_config.py

--- superset_config.py
+++ superset_config.py
@@ -6,4 +6,11 @@ if raw_url and raw_url.startswith('postgresql://'):
     raw_url = raw_url.replace('postgresql://', 'postgresql+psycopg2://', 1)
 SQLALCHEMY_DATABASE_URI = raw_url
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+AUTH_ROLE_PUBLIC = 'Public'
 PUBLIC_ROLE_LIKE_GAMMA = True
+ENABLE_AUTH_EVENT_LOG = False
+SESSION_COOKIE_SECURE= False
+SESSION_COOKIE_SAMESITE= 'Lax'
+WTF_CSRF_ENABLED= False
+WTF_CSRF_EXEMPT_LIST=['*']
